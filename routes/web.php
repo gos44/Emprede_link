7c1b4638cd59271d86c425cb33e8135586625a64
Dayron Hernandez <dayronhernandezparedes@gmail.com>
2024-06-05 08:21:49 -0500

    subiendo registro inversionistas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Inversionista;
+use App\Http\Controllers\InversionistaController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('inversionistas/create', [InversionistaController::class, 'create'])->name('inversionistas.create');
+Route::post('inversionistas/store', [InversionistaController::class, 'store'])->name('inversionistas.store');
+Route::get('inversionistas/listar', [InversionistaController::class, 'index'])->name('inversionistas.index');
+Route::delete('inversionistas/{inversionista}', [InversionistaController::class, 'destroy'])->name('inversionistas.destroy');
+Route::get('inversionistas/{inversionista}', [InversionistaController::class, 'show'])->name('inversionistas.show');
+
+Route::put('inversionistas/{inversionista}',[InversionistaController::class,'update'])->name('inversionistas.update');
+Route::get('inversionistas/{inversionista}/editar',[InversionistaController::class,'edit'])->name('inversionistas.edit');
