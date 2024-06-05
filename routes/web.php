786004d005552c644687402192c0813d510f2397
Sebastian-paz <esdp5432@gmail.com>
2024-06-05 11:57:14 -0500

    subiendo resenas

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\controllerconsultar_lista;
+use App\Http\Controllers\ResenaController;
 use App\Http\Controllers\controllerconsultar_todo;
 
 
+
+
+
+//
+Route::get('resena/create',[ResenaController::class,'create']);
+Route::post('resenas/store', [ResenaController::class,'store'])->name('resenas.store');
+Route::get('resena/listar',[ResenaController::class,'index'])->name('resena.index');
+Route::get('resena/{resena}',[ResenaController::class,'show'])->name('resena.show');
+Route::put('resena/{resena}',[ResenaController::class,'update'])->name('resena.update');//nuevo
+Route::delete('resena/{resena}',[ResenaController::class,'destroy'])->name('resena.destroy');
+Route::get('resena/{resena}/editar',[ResenaController::class,'edit'])->name('resena.edit');
 
 Route::get('/frlista',[controllerconsultar_lista::class,'formulario_lista']);
 Route::post('/data_list',[controllerconsultar_lista::class,'data_list'])->name('data_lista');
