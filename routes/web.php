65236feeab28534a6038eecb4a4d499942c5d030
k2035ortiz <kevinalexisortizcalvahe@gmail.com>
2024-06-05 11:59:24 -0500

    subiendo tabla publicar emprendimiento

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\controllerconsultar_lista;
+use App\Http\Controllers\PublicarEmprendimientoController;
 use App\Http\Controllers\controllerconsultar_todo;
+
+
+
+
+
+
+
+//plicar_emprendimiento k
+Route::get('trabajo/tabla',[PublicarEmprendimientoController::class,'create']);
+Route::post('Publicar_Emprendimiento/store', [PublicarEmprendimientoController::class,'store'])->name('Publicar_Emprendimiento.store');
+Route::get('trabajo/listar',[PublicarEmprendimientoController::class,'index'])->name('trabajo.index');
+Route::get('trabajo/{trabajo}',[PublicarEmprendimientoController::class,'show'])->name('trabajo.show');
+Route::put('trabajo/{trabajo}',[PublicarEmprendimientoController::class,'update'])->name('trabajo.update');//nuevo
+Route::delete('trabajo/{trabajo}',[PublicarEmprendimientoController::class,'destroy'])->name('trabajo.destroy');
+Route::get('trabajo/{trabajo}/editar',[PublicarEmprendimientoController::class,'edit'])->name('trabajo.edit');
 
 
 
