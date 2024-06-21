d50a69dce16235d6f1d5ca3751103f05b0e0820e
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-06-21 17:47:50 -0500

    Merge con main

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\controllerconsultar_lista;
 use App\Http\Controllers\PublicarEmprendimientoController;
-use App\Http\Controllers\controllerconsultar_todo;
-use App\Http\Controllers\ConsultListController;
-use App\Http\Controllers\ConsultEverythingController;
-
-
-
-
 
 
 //plicar_emprendimiento k
-Route::get('trabajo/tabla',[PublicarEmprendimientoController::class,'create']);
+Route::get('/trabajo/tabla',[PublicarEmprendimientoController::class,'create']);
 Route::post('Publicar_Emprendimiento/store', [PublicarEmprendimientoController::class,'store'])->name('Publicar_Emprendimiento.store');
 Route::get('trabajo/listar',[PublicarEmprendimientoController::class,'index'])->name('trabajo.index');
 Route::get('trabajo/{trabajo}',[PublicarEmprendimientoController::class,'show'])->name('trabajo.show');
