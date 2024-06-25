6191249674278d893a8687a3da0c4e06333e4d5e
angel2005gg <bonilllapalechormiguelangel@gmail.com>
2024-06-25 10:51:51 -0500

    emprendedor/emprendimiento a main

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\controllerconsultar_lista;
-use App\Http\Controllers\controllerconsultar_todo;
+
 use App\Http\Controllers\EmprendedorController;
 use App\Http\Controllers\EmprendimientoController;
 
 
-Route::get('/frlista',[controllerconsultar_lista::class,'formulario_lista']);
-Route::post('/data_list',[controllerconsultar_lista::class,'data_list'])->name('data_lista');
-Route::get('/listarconsulta', [controllerconsultar_lista::class,'listar_lista'])->name('listar_lista'); 
-Route::delete('/{consultar_lista}', [controllerconsultar_lista::class,'destroy'])->name('destroy'); 
-Route::post('/consultalist',[controllerconsultar_lista::class,'data_list'])->name('consultar_lista');
-Route::get('/{lista}',[controllerconsultar_lista::class,'show'])->name('show_consultarlista');
+
 
 
 //Emprendedor
