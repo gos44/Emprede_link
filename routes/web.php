eddf7258382be474d7f8b45ba1a8c8e17df18dff
sebastian delgado <esdp5432@gmail.com>
2024-06-24 21:30:23 -0500

    agregando resenas

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\controllerconsultar_lista;
 use App\Http\Controllers\ResenaController;
-use App\Http\Controllers\controllerconsultar_todo;
+
 
 
 
@@ -18,9 +17,4 @@ Route::put('resena/{resena}',[ResenaController::class,'update'])->name('resena.u
 Route::delete('resena/{resena}',[ResenaController::class,'destroy'])->name('resena.destroy');
 Route::get('resena/{resena}/editar',[ResenaController::class,'edit'])->name('resena.edit');
 
-Route::get('/frlista',[controllerconsultar_lista::class,'formulario_lista']);
-Route::post('/data_list',[controllerconsultar_lista::class,'data_list'])->name('data_lista');
-Route::get('/listarconsulta', [controllerconsultar_lista::class,'listar_lista'])->name('listar_lista'); 
-Route::delete('/{consultar_lista}', [controllerconsultar_lista::class,'destroy'])->name('destroy'); 
-Route::post('/consultalist',[controllerconsultar_lista::class,'data_list'])->name('consultar_lista');
-Route::get('/{lista}',[controllerconsultar_lista::class,'show'])->name('show_consultarlista');
+
